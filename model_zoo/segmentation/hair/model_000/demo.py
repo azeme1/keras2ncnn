6ec0de937abb8ff6e77a4cb9b3b3e585860c08be
azeme1 <alexander.lemeza@oxagile.com>
2020-12-25 18:22:19 +0300

    [FIX] Fix pre-processing os the demo sample

--- model_zoo/segmentation/hair/model_000/demo.py
+++ model_zoo/segmentation/hair/model_000/demo.py
@@ -18,11 +18,13 @@ src_y, src_x = frame.shape[:2]
 
 
 input_data = cv2.resize(frame, (target_x, target_y))
-mean_value = input_data.reshape((-1, 3)).mean(0)
-inv_std_value = 1. / (1.e-5 + input_data.reshape((-1, 3)).std(0))
+mean_value = input_data.reshape((-1, 3)).mean(0)[::-1]
+inv_std_value = 1. / (1.e-5 + input_data.reshape((-1, 3)).std(0))[::-1]
 
 mat_in = ncnn.Mat.from_pixels_resize(frame, ncnn.Mat.PixelType.PIXEL_RGB, src_x, src_y, target_x, target_y)
 mat_in.substract_mean_normalize(mean_value, inv_std_value)
+
+mean, std = cv2.meanStdDev(frame)
 
 keras_in = (np.array(input_data)[None] - mean_value)*inv_std_value
 keras_out = keras_model.predict(keras_in)[0, ..., 0]
